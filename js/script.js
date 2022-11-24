b9837876e3e127e464432b016d66e96dac9ae244
Nuriddin70 <nuriddin3050@gmail.com>
2022-11-24 20:12:34 +0900

    done with modal

--- js/script.js
+++ js/script.js
@@ -104,19 +104,52 @@ window.addEventListener("DOMContentLoaded", () => {
 
   //Modal
 
-  const modalTrigger = document.querySelector('[data-modal]'),
-    modal = document.querySelector('.modal'),
-    modalCloseBtn = document.querySelector('[data-close]')
+  const modalTrigger = document.querySelectorAll("[data-modal]"),
+    modal = document.querySelector(".modal"),
+    modalCloseBtn = document.querySelector("[data-close]");
 
-  modalTrigger.addEventListener('click', () => {
-    modal.classList.add('show')
-    modal.classList.remove('hide')
-    document.body.style.overflow = 'hidden' 
-  })
+  function closeModal() {
+    modal.classList.add("hide");
+    modal.classList.add("show");
+    document.body.style.overflow = "";
+  }
 
-  modalCloseBtn.addEventListener('click', () => {
-    modal.classList.add('hide')
-    modal.classList.add('show')
-    document.body.style.overflow = '' 
-  })
+  function openModal() {
+    modal.classList.add("show");
+    modal.classList.remove("hide");
+    document.body.style.overflow = "hidden";
+    clearInterval(modalTimerId);
+  }
+
+  modalTrigger.forEach((item) => {
+    item.addEventListener("click", openModal);
+  });
+
+  modalCloseBtn.addEventListener("click", closeModal);
+
+  modal.addEventListener("click", (e) => {
+    if (e.target === modal) {
+      closeModal();
+    }
+  });
+
+  document.addEventListener("keydown", (e) => {
+    if (e.code === "Escape" && modal.classList.contains("show")) {
+      closeModal();
+    }
+  });
+
+  const modalTimerId = setTimeout(openModal, 6000);
+
+  function showModalByScroll() {
+    if (
+      window.pageYOffset + document.documentElement.clientHeight >=
+      document.documentElement.scrollHeight
+    ) {
+      openModal();
+      window.removeEventListener("scroll", showModalByScroll);
+    }
+  }
+
+  window.addEventListener("scroll", showModalByScroll);
 });
