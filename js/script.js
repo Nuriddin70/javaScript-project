943840af294a0bca1e81e538cfff1addc0bb2c71
Nuriddin70 <nuriddin3050@gmail.com>
2023-01-06 20:11:22 +0900

    add corusel func

--- js/script.js
+++ js/script.js
@@ -192,40 +192,24 @@ window.addEventListener('DOMContentLoaded', () => {
     }
   }
 
-  new MenuCard(
-    'img/tabs/1.png',
-    'usual',
-    'Plan "Usual"',
-    'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugit nesciunt facere, sequi exercitationem praesentium ab cupiditatebeatae debitis perspiciatis itaque quaerat id modi corporis delectus ratione nobis harum voluptatum in.',
-    10,
-    '.menu .container'
-  ).render()
-
-  new MenuCard(
-    'img/tabs/2.jpg',
-    'plan',
-    'Plan “Premium”',
-    'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugit nesciunt facere, sequi exercitationem praesentium ab cupiditatebeatae debitis perspiciatis itaque quaerat id modi corporis delectus ratione nobis harum voluptatum in.',
-    20,
-    '.menu .container',
-    'menu__item'
-  ).render()
-
-  new MenuCard(
-    'img/tabs/3.jpg',
-    'vip',
-    'Plan VIP',
-    'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugit nesciunt facere, sequi exercitationem praesentium ab cupiditatebeatae debitis perspiciatis itaque quaerat id modi corporis delectus ratione nobis harum voluptatum in.',
-    30,
-    '.menu .container',
-    'menu__item'
-  ).render()
+  axios.get('http://localhost:3000/menu').then((data) => {
+    data.data.forEach(({ img, altimg, title, descr, price }) => {
+      new MenuCard(
+        img,
+        altimg,
+        title,
+        descr,
+        price,
+        '.menu .container'
+      ).render()
+    })
+  })
 
   // Form
   const forms = document.querySelectorAll('form')
 
   forms.forEach((form) => {
-    postData(form)
+    bindPostData(form)
   })
 
   const msg = {
@@ -234,7 +218,23 @@ window.addEventListener('DOMContentLoaded', () => {
     failure: 'Something went wrong',
   }
 
-  function postData(form) {
+  async function postData(url, data) {
+    const res = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: data,
+    })
+
+    if (!res.ok) {
+      throw new Error(`Could not fetch ${url}, status: ${res.status}`)
+    }
+
+    return await res.json()
+  }
+
+  function bindPostData(form) {
     form.addEventListener('submit', (e) => {
       e.preventDefault()
 
@@ -248,19 +248,9 @@ window.addEventListener('DOMContentLoaded', () => {
 
       const formData = new FormData(form)
 
-      const obj = {}
-      formData.forEach((val, key) => {
-        obj[key] = val
-      })
+      const json = JSON.stringify(Object.fromEntries(formData.entries()))
 
-      fetch('server.php', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(obj),
-      })
-        .then((data) => data.text())
+      postData('http://localhost:3000/request', json)
         .then((data) => {
           console.log(data)
           showThanksModal(msg.success)
@@ -298,4 +288,164 @@ window.addEventListener('DOMContentLoaded', () => {
       closeModal()
     }, 4000)
   }
+
+  // Slider
+
+  const slides = document.querySelectorAll('.offer__slide'),
+    next = document.querySelector('.offer__slider-next'),
+    prev = document.querySelector('.offer__slider-prev'),
+    total = document.querySelector('#total'),
+    current = document.querySelector('#current'),
+    slidesWrapper = document.querySelector('.offer__slider-wrapper'),
+    slidesField = document.querySelector('.offer__slider-inner'),
+    width = window.getComputedStyle(slidesWrapper).width,
+    sldier = document.querySelector('.offer__slider')
+
+  let slideIndex = 1
+  let offset = 0
+
+  // ------*************************************************---------------------
+  //                CAROUSEL SLIDER
+  // ------*************************************************---------------------
+
+  if (slides.length < 10) {
+    total.textContent = `0${slides.length}`
+    current.textContent = `0${slideIndex}`
+  } else {
+    total.textContent = slides.length
+    current.textContent = slideIndex
+  }
+
+  slidesField.style.width = 100 * slides.length + '%'
+  slidesField.style.display = 'flex'
+  slidesField.style.transition = '.5s ease all'
+  slidesWrapper.style.overflow = 'hidden'
+
+  slides.forEach((slide) => {
+    slide.style.width = width
+  })
+
+  const indicators = document.createElement('ol')
+  const dots = []
+  indicators.classList.add('carousel-indicators')
+  sldier.append(indicators)
+
+  for (let i = 0; i < slides.length; i++) {
+    const dot = document.createElement('li')
+    dot.setAttribute('data-slide-to', i + 1)
+    dot.classList.add('carousel-dot')
+    if (i == 0) {
+      dot.style.opacity = 1
+    }
+    indicators.append(dot)
+    dots.push(dot)
+  }
+
+  next.addEventListener('click', () => {
+    if (offset == +width.slice(0, width.length - 2) * (slides.length - 1)) {
+      offset = 0
+    } else {
+      offset += +width.slice(0, width.length - 2)
+    }
+    slidesField.style.transform = `translateX(-${offset}px)`
+
+    if (slideIndex == slides.length) {
+      slideIndex = 1
+    } else {
+      slideIndex++
+    }
+
+    if (slides.length < 10) {
+      current.textContent = `0${slideIndex}`
+    } else {
+      current.textContent = slideIndex
+    }
+
+    dots.forEach((dot) => (dot.style.opacity = '.5'))
+    dots[slideIndex - 1].style.opacity = 1
+  })
+
+  prev.addEventListener('click', () => {
+    if (offset == 0) {
+      offset = +width.slice(0, width.length - 2) * (slides.length - 1)
+    } else {
+      offset -= +width.slice(0, width.length - 2)
+    }
+    slidesField.style.transform = `translateX(-${offset}px)`
+
+    if (slideIndex == 1) {
+      slideIndex = slides.length
+    } else {
+      slideIndex--
+    }
+
+    if (slides.length < 10) {
+      current.textContent = `0${slideIndex}`
+    } else {
+      current.textContent = slideIndex
+    }
+
+    dots.forEach((dot) => (dot.style.opacity = '.5'))
+    dots[slideIndex - 1].style.opacity = 1
+  })
+
+  dots.forEach((dot) => {
+    dot.addEventListener('click', (e) => {
+      const slideTo = e.target.getAttribute('data-slide-to')
+
+      slideIndex = slideTo
+      offset = +width.slice(0, width.length - 2) * (slideTo - 1)
+      slidesField.style.transform = `translateX(-${offset}px)`
+
+      if (slides.length < 10) {
+        current.textContent = `0${slideIndex}`
+      } else {
+        current.textContent = slideIndex
+      }
+
+      dots.forEach((dot) => (dot.style.opacity = '.5'))
+      dots[slideIndex - 1].style.opacity = 1
+    })
+  })
+
+  // ------*************************************************---------------------
+  //                EASY SLIDER
+  // ------*************************************************---------------------
+
+  // showSlides(slideIndex)
+
+  // if(slides.length < 10){
+  //   total.textContent = `0${slides.length}`
+  // }else{
+  //   total.textContent = slides.length
+  // }
+
+  // function showSlides(idx){
+  //   if(idx > slides.length){
+  //     slideIndex = 1
+  //   }
+  //   if(idx < 1){
+  //     slideIndex = slides.length
+  //   }
+  //   slides.forEach(item => item.style.display = 'none')
+  //   slides[slideIndex - 1].style.display = 'block'
+
+  //   if(slides.length < 10){
+  //     current.textContent = `0${slideIndex}`
+  //   }else{
+  //     current.textContent = slideIndex
+  //   }
+  // }
+
+  // function plusSlides(idx) {
+  //   showSlides(slideIndex += idx)
+  // }
+
+  // next.addEventListener('click', () => {
+  //   plusSlides(1)
+  // })
+
+  // prev.addEventListener('click', () => {
+  //   plusSlides(-1)
+  // })
 })
